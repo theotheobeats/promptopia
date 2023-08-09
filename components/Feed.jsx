d5ca0474932c3f8597e7c5dfc020fd412b5713e4
Theo <asakvsa.idn@gmail.com>
2023-08-09 20:15:15 +0700

    update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import PromptCard from './PromptCard'
 
-const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data, handleTagClick, handleNameClick }) => {
   return (
     <div className='mt-16 prompt_layout'>
       {data.map((post) => (
@@ -11,6 +11,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
           key={post._id}
           post={post}
           handleTagClick={handleTagClick}
+          handleNameClick={handleNameClick}
         />
       ))}
     </div>
@@ -22,19 +23,42 @@ const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchText(value);
 
+    if (value === '') {
+      fetchPosts();
+    }
+
+    const filteredResult = filterData(value);
+    setPosts(filteredResult);
+  }
+
+  const filterData = (value) => {
+    return posts.filter(post =>
+      (typeof post.creator.username === 'string' && post.creator.username.toLowerCase().includes(value.toLowerCase())) ||
+      (typeof post.prompt === 'string' && post.prompt.toLowerCase().includes(value.toLowerCase())) ||
+      (typeof post.tag === 'string' && post.tag.toLowerCase().includes(value.toLowerCase()))
+    );
+  }
+
+  const fetchPosts = async () => {
+    const response = await fetch('api/prompt');
+    const data = await response.json();
+
+    setPosts(data);
+  }
+
+  const handleTagClick = (tag) => {
+    setSearchText(tag);
+    handleSearchChange({ target: { value: tag } });
+  }
+
+  const handleNameClick = (tag) => {
+    
   }
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('api/prompt');
-      const data = await response.json();
-
-      setPosts(data);
-    }
-
-    console.log(posts);
-
     fetchPosts();
   }, [])
 
@@ -54,6 +78,8 @@ const Feed = () => {
       </form>
       <PromptCardList
         data={posts}
+        handleNameClick={handleNameClick}
+        handleTagClick={handleTagClick}
       />
     </section>
   )
